b3c57fec1e4ece7a659a3891a84daedf9a5a680b
BrniceKwaw <2425403497@live.gctu.edu.gh>
2026-05-31 11:59:48 +0000

    Add files via upload

---
+++ initials.py.py
@@ -0,0 +1,33 @@
+# File: initials.py
+# Description: BSC COMPUTER SCIENCE
+# Assignment Number: 2
+#
+# Name: Bernice Kwaw Araba
+# STUDENT ID:  2425403497
+# Email: 2425403497.live.gctu.edu.gh
+# Grader: <Carolyn OR Emma OR Ahmad>
+#
+# On my honor, Bernice Kwaw Araba, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+    # Print initials BKA in  large block letters (12 wide x 10 tall each)
+
+    print()
+    print("...BKA")
+    print()
+    print("...BBBBBBBBBB..........KKKK....KKKK............AAAA............")
+    print("...BBBB....BBBB........KKKK...KKKK............AAAAAA...........")
+    print("...BBBB....BBBB........KKKK..KKKK............AAAA.AAA..........")
+    print("...BBBB....BBBB........KKKK.KKKK............AAAA...AAA.........")
+    print("...BBBBBBBBB...........KKKKKKKK............AAAA.....AAA........")
+    print("...BBBBBBBBBBBB........KKKKKKKK............AAAAAAAAAAAA........")
+    print("...BBBB....BBBB........KKKK.KKKK...........AAAA.....AAA........")
+    print("...BBBB....BBBB........KKKK..KKKK..........AAAA.....AAA........")
+    print("...BBBB....BBBB...**...KKKK...KKKK....**...AAAA.....AAA........")
+    print("...BBBBBBBBBB.....**...KKKK....KKKK...**...AAAA.....AAA........")
+    print()
+
+
+main()
